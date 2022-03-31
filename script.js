77241007f2ddba92b06029ad26a73622324e2e8d
CodeGuiFs <100089821+CodeGuiFs@users.noreply.github.com>
2022-03-31 01:52:29 -0300

    Alteração completa no código!
    Mudança nos estilos do CSS
    Adição de Funções
    Organização do HTML

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function sub(){
-    window.alert("Você foi cadastrado com sucesso!")
+var txtname = document.getElementById('txtname')
+var txtpass = document.getElementById('txtpass')
+var list = ["cloud","CLOUD", "lx2018", "LX2018", "kiwi", "KIWI"]
+
+function checkName(){
+    if(txtname.value == ""){
+        alert('O campo "Nome" é Obrigatório')
+    }else{
+        for(var pos in list){
+            if(txtname.value == list[pos]){
+                return alert(`O nome ${txtname.value} é inválido!`)
+            }else{
+            }
+        }
+
+    }
+}
+
+function checkPass(){
+    const regex = /^(?=.*[a-z])(?=.*\d)(?=.*[!#@$%&*+=-]).{6,16}$/;
+    let check = regex.test(txtpass.value)
+    if(check == true){
+        return alert("Você foi cadastrado com sucesso")
+    }else{
+        return alert("Revise os requisitos de Senha")
+    }
+}
+
+function runCode(){
+    checkName()
+    checkPass()
 }
